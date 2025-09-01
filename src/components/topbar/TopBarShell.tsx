96eb04d3c43046d0d572a11171e5dc38c791a713
sklores <sklores@gmail.com>
2025-09-01 08:58:18 -0400

    topbarshell update

--- src/components/topbar/TopBarShell.tsx
+++ src/components/topbar/TopBarShell.tsx
@@ -1,7 +1,7 @@
 // src/components/topbar/TopBarShell.tsx
 import BorderFrame from "./BorderFrame/BorderFrame";
 import ClientLogo from "./ClientLogo/ClientLogo";
-import ScenicStage from "./ScenicStage";
+import ScenicStage from "./ScenicStage/index";
 import TabBar from "./TabBar/TabBar";
 
 export default function TopBarShell() {
